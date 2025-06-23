76930835b7359fac0c6f779391b3a04892b4de8c
App Generator <noreply@appgenerator.com>
2025-06-22 20:48:23 -0500

    Add client/src/components/Button.js

---
+++ client/src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button 
+      className="button" 
+      onClick={() => onClick(children)}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
